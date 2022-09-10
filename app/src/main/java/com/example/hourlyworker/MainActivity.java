c2a2f7543fc3ad122ef4778fa18519be2d1be4f0
Nitzan <bresler.nitzan@gmail.com>
2022-09-10 23:28:25 +0300

    Some black magic with shared prefs

--- app/src/main/java/com/example/hourlyworker/MainActivity.java
+++ app/src/main/java/com/example/hourlyworker/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.hourlyworker;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
 	private static double timeSaved = 0;
 	private static long startTime;
 	private MaterialButton startStopButton, resetButton;
+	private Spinner spinner;
 	private TextView moneyView, timeView;
+	SharedPreferences sharedPreferences;
 	private EditText rateView;
 	private static final DecimalFormat df = new DecimalFormat("0.00");
 
@@ -84,20 +93,56 @@ public class MainActivity extends AppCompatActivity {
 		setupResetButton();
 	}
 
-	private void setup() {
+	private void findViews() {
 		startStopButton = findViewById(R.id.start_stop);
 		resetButton = findViewById(R.id.reset);
 		moneyView = findViewById(R.id.money);
 		timeView = findViewById(R.id.timer);
 		rateView = findViewById(R.id.rate);
-		setupButtons();
+		spinner = findViewById(R.id.spinner);
+
+		sharedPreferences = getPreferences(MODE_PRIVATE);
+		rateView.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+				sharedPreferences.edit().putString("hourlyRate", editable.toString()).apply();
+			}
+
+		});
+		rateView.setText(sharedPreferences.getString("hourlyRate", "50"));
 	}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		setup();
+	private void setupDropdown() {
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		spinner.setAdapter(adapter);
+		sharedPreferences = getPreferences(MODE_PRIVATE);
+		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@SuppressLint("SetTextI18n")
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+				sharedPreferences.edit().putInt("currency", i).apply();
+				moneyView.setText(moneyView.getText().toString().substring(0, moneyView.getText().toString().length() - 1) + spinner.getSelectedItem().toString());
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
+
+			}
+		});
+		sharedPreferences = getPreferences(MODE_PRIVATE);
+		spinner.setSelection(sharedPreferences.getInt("currency", 5));
+	}
+
+	private void setupHandler() {
 		Handler handler = new Handler();
 		int delay = 1;
 		handler.postDelayed(new Runnable() {
@@ -105,11 +150,26 @@ public class MainActivity extends AppCompatActivity {
 			public void run() {
 				if (isRunning) {
 					moneyEarned = getMoneyEarned();
-					moneyView.setText(getAllMoney() + " $");
+					moneyView.setText(getAllMoney() + " " + spinner.getSelectedItem().toString());
 					timeView.setText(getTimerPassed());
 				}
 				handler.postDelayed(this, delay);
 			}
 		}, delay);
+	}
+
+	private void setup() {
+		findViews();
+		setupDropdown();
+		setupButtons();
+		setupHandler();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setup();
+
 	}
 }
